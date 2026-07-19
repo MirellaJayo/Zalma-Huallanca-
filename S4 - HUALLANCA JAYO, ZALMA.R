dd5d8ee83ba96a3ea30459f9004bd26112528259
MirellaJayo <mirellajayo1909@gmail.com>
2026-07-19 17:12:18 -0500

    Add files via upload

---
+++ S4 - HUALLANCA JAYO, ZALMA.R
@@ -0,0 +1,16 @@
+#SEMANA 4
+#INSTALACION DE PAQUETES
+install.packages("ggplot2")
+library(ggplot2)
+
+install.packages("dplyr")
+library(dplyr)
+
+install.packages("readr")
+library(readr)
+
+install.packages("tidyr")
+library(tidyr)
+
+install.packages("psych")
+library(psych)
